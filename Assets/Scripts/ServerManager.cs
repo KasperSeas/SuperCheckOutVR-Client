53d947390c53a8d0b5ac67f80f2687038d7df518
KasperSeas <kevinravimowers@gmail.com>
2016-10-22 14:13:32 -0700

    init ServerManager script

---
+++ Assets/Scripts/ServerManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+//using JSONObject; 
+
+public class ServerManager : MonoBehaviour {
+
+	private string results;
+
+	public delegate void OnCompleteDelegate();
+	public void OnComplete(string results)
+	{
+		Debug.Log (results);
+	}
+
+	void Start(){
+		GET ("localhost:6499/product/findAll");
+	}
+
+	public WWW GET(string url) {
+		
+
+		WWW www = new WWW (url);
+		StartCoroutine (WaitForRequest (www));
+		return www;
+	}
+
+	private IEnumerator WaitForRequest(WWW www) {
+		yield return www;
+		// check for errors
+		if (www.error == null) {
+			results = www.text;
+			OnComplete(results);
+		} else {
+			Debug.Log (www.error);
+		}
+	}
+
+
+}
