22606292d950d67a316c6cc3f00d02847e28056d
KasperSeas <kevinravimowers@gmail.com>
2016-10-22 16:40:04 -0700

    PM destroy works!

--- Assets/Scripts/ProductManager.cs
+++ Assets/Scripts/ProductManager.cs
@@ -11,11 +11,11 @@ public class ProductManager : MonoBehaviour {
 		sm = GetComponent<ServerManager> ();
 //		findAllItems ();
 		Product prod = new Product("Blue Paint", 8.0f);
-//		blue paint id: 580bf2867ca1704f69b6432a
-
 //		createItem(prod);
-		updateItem("580bf2867ca1704f69b6432a", prod);
-
+//		updateItem("580bf8557ca1704f69b6432d", prod);
+//		findItem("580bf2867ca1704f69b6432a");
+//		destroyItem("580bf8557ca1704f69b6432d");
+		findAllItems();
 	}
 
 	void createItem (Product product) {
@@ -23,6 +23,10 @@ public class ProductManager : MonoBehaviour {
 		dict.Add ("name", product.name);
 		dict.Add ("price", product.price.ToString());
 		sm.POST (supercheckoutvr+"product/create", dict);
+	}
+
+	void findItem (string id) {
+		sm.GET(supercheckoutvr+"product/findOne?_id="+id);	
 	}
 	
 	void updateItem  (string id, Product product) {
@@ -34,6 +38,10 @@ public class ProductManager : MonoBehaviour {
 
 	void findAllItems() {
 		sm.GET(supercheckoutvr+"product/findAll");
+	}
+
+	void destroyItem(string id) {
+		sm.GET (supercheckoutvr+"product/destroy?_id="+id);
 	}
 
 
