21ef7faa10c4488bc2a82e70b8426c2512e1bc47
Andrew Aquino <andrewaquino118@yahoo.com>
2016-10-22 23:27:32 -0700

    added product description

---
+++ Assets/ProductDescription.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProductDescription : MonoBehaviour {
+
+	public string name = "";
+	public float price = 0.0f;
+	public string description = "";
+	public string brand = "";
+
+	Product product;
+
+	// Use this for initialization
+	void Start () {
+		string price = string.Format("{0:0.00}", this.price);    
+		Debug.Log ("Added product: " + brand + " " + name + ", $" + price + ", " + description);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
